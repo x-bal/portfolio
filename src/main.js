76121e0dec27fc85d0595501c6071446745d4124
Muhamad iqbal Ilham Rahayu <70595626+x-bal@users.noreply.github.com>
2021-12-13 15:45:42 +0700

    files

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 
 import axios from 'axios'
-axios.defaults.baseURL = "http://127.0.0.1:8000/api/"
+axios.defaults.baseURL = "https://portfolio.mxbal.my.id/api/"
 document.title = "Muhammad Iqbal"
 
 createApp(App).use(router).use(Particles).mount('#app')
